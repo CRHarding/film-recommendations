b74dbc5c948055e1c0279b6dd5e045644e172be2
CR Harding <harding@brooklynfoundry.com>
2019-05-11 14:06:26 -0400

    [Exception Controller] Hot fix for improper error handling.

--- src/main/java/co/ga/freshpotatoes/ExceptionController.java
+++ src/main/java/co/ga/freshpotatoes/ExceptionController.java
@@ -1,5 +1,6 @@
 package co.ga.freshpotatoes;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
@@ -12,14 +13,14 @@ import java.util.logging.Logger;
 @ControllerAdvice
 public class ExceptionController {
     @ExceptionHandler(Exception.class)
-    public ModelAndView handleError(HttpServletRequest request, Exception e)   {
+    public ResponseEntity<String> handleError(HttpServletRequest request, Exception e)   {
         Logger.getLogger(getClass().getName()).log(Level.SEVERE, "Request: " + request.getRequestURL() + " raised " + e);
-        return new ModelAndView("error");
+        return ResponseEntity.ok("An internal error occured");
     }
 
     @ExceptionHandler(NoHandlerFoundException.class)
-    public ModelAndView handleError404(HttpServletRequest request, Exception e)   {
+    public ResponseEntity<String> handleError404(HttpServletRequest request, Exception e)   {
         Logger.getLogger(getClass().getName()).log(Level.SEVERE, "Request: " + request.getRequestURL() + " raised " + e);
-        return new ModelAndView("404");
+        return ResponseEntity.ok("That route does not exist");
     }
 }
