84ace5f8a74972d653cd049eabf87eb2407f6857
CR Harding <harding@brooklynfoundry.com>
2019-05-11 12:40:43 -0400

    [Web] [Films Controller] Set remote api url, added in json parser maven dependency to aid in parsing the response, added json exception handling to method signature.

--- src/main/java/co/ga/freshpotatoes/web/FilmsController.java
+++ src/main/java/co/ga/freshpotatoes/web/FilmsController.java
@@ -1,10 +1,13 @@
 package co.ga.freshpotatoes.web;
 
+import org.json.JSONException;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.json.JSONArray;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -15,6 +18,7 @@ import co.ga.freshpotatoes.domain.entity.Film;
 import co.ga.freshpotatoes.domain.entity.Genre;
 import co.ga.freshpotatoes.domain.repository.FilmRepository;
 import co.ga.freshpotatoes.domain.repository.GenreRepository;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class FilmsController {
@@ -26,7 +30,7 @@ public class FilmsController {
   @RequestMapping(value="/films/{film_id}/recommendations", method=RequestMethod.GET)
   public Set<Film> recommendations(@PathVariable Long film_id,
                                    @RequestParam (required = false) Integer offset,
-                                   @RequestParam (required = false) Integer limit) {
+                                   @RequestParam (required = false) Integer limit) throws JSONException {
 
     Film film = this.filmRepository.findById(film_id);
     Genre genre = this.genreRepository.findOne(film.getGenre().getId());
@@ -48,6 +52,14 @@ public class FilmsController {
         if (filteredFilmsByDate == null) {
           filteredFilmsByDate = new HashSet<>();
         }
+
+        final String uri = "http://credentials-api.generalassemb.ly/4576f55f-c427-4cfc-a11c-5bfe914ca6c1?films=" + f.getId();
+
+        RestTemplate restTemplate = new RestTemplate();
+        String resp = restTemplate.getForObject(uri, String.class);
+
+        JSONArray reviewObject = new JSONArray(resp);
+
 
         filteredFilmsByDate.add(f);
       }
